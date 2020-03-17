cd0bd00bd7e1ceed45d7398165902d55e3ba2e4a
ClintSouthwood <apinkerpanther@gmail.com>
2020-03-17 02:57:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Westwoods RCON Bot [![forthebadge](https://forthebadge.com/images/badges/made-with-javascript.svg)](https://forthebadge.com)
+
+Buenoes, noches. This is a Discord.JS bot, which if setup correctly can relay messages from your server's RCON to a specified discord. This bot also tracks users and the amount of time since the server's wipe, displaying it (if enabled) as the bot's status.
+
+
+# How does it work?
+
+This bot uses ``ws`` to create a WebSocket, which connects to the server. From there whenever a message is recieved, those messages are sorted and based on the conditions provided gives them a unique embed and color. To track players, when the bot connects it sends a command to the RCON with a unique identifier. The RCON then responds to the command with a message, that has the same unique identifier. This message is sorted based on it's  unique identifier, and every time it is received redefines the amount of players online, maximum, queued, and time since wipe.
+
+## Configuration
+Item | Definition
+--- | ---
+TOKEN=COOLASSTOKENHERE |
+IP=SERVERIP | Your server IP address.
+PORT=SERVERPORT | Your **RCON** Port!
+PASS=RCONPASSWORD | RCON Password, defined in your server startup parameters.
+CONNECT_CHAN=689205495386734661 | Channel ID for where the connection and close messages are sent.
+CONNECT_MSG=``The RCON connection has been opened.`` | Message that is sent when the connection is opened.
+CLOSE_MSG=``The RCON connection has been closed.`` | Message that is sent when the connection is closed (If you shutdown the bot it will not display this, only when the server is shutdown or restarted).
+RELOAD=15000 | Time in milliseconds between server info updates (getting player count), default is 15 seconds.
+PLAYER_CHAN=689283942767263752 |
+SERVER_COLOR=#fdfdfd | Embed color for all server messages.
+SERVER_CHAN=689205495386734661 | Channel ID for server messages (It's a lot of spam), delete or create some random number to disable.
+ABUSE_COLOR=#EFD298 | Embed color for possibly abusive commands such as, give, teleport (not working at the moment, or killplayer).
+ABUSE_CHAN=689205495386734661 | Channel ID for possible abusive commands, delete or create some random number to disable.
+PUNISH_COLOR=#C94646 | Embed color for punishments
+PUNISH_CHAN=689205495386734661 | Channel ID for punishments (mute,kick,ban,unmute,unban), delete or create some random number to disable.
+WARNING_COLOR=#fff891 | Embed color for warnings, typically server or Oxide warnings.
+WARNING_CHAN=689205495386734661 | Channel ID for warning, delete or create some random number to disable.
+CHAT_COLOR=#98DCEF | Global chat embed color
+TEAM_COLOR=#A4EF98 | Team chat embed color
+PRIVATE_COLOR=#EB98EF | Private message embed color
+SCHAT_COLOR=#B298EF | Server chat embed color
+CHAT_CHAN=689205495386734661 | Channel ID of where all chat messages go to.
+INCLUDE_WIPED_FROM=LeaveBlankToDisable |Whether or not you want the wipe time to be shown in the status. Delete **LeaveBlankToDisable** for it to be disabled.
+
+##
---
+++ westwood.js
@@ -0,0 +1,169 @@
+let Discord = require('discord.js');
+let WebSocket = require('ws')
+let moment = require('moment')
+require('dotenv').config();
+
+let c = new Discord.Client();
+
+let ip = process.env.IP;
+let port = process.env.PORT;
+let pass = process.env.PASS;
+let url = 'ws://' + ip + ':' + port + '/' + pass;
+
+let client = new WebSocket(url)
+
+client.onopen = function(event) {
+    console.log(`Connected to ${url}`)
+    setTimeout(() => {
+        c.channels.fetch(process.env.CONNECT_CHAN)
+            .then(channel => channel.send(`${process.env.CONNECT_MSG}`))
+    }, 2000);
+
+    setInterval(() => {
+        client.send(JSON.stringify({
+            Identifier: 300,
+            Message: 'serverinfo',
+            Username: 'Clint Westwood'
+        }))
+    }, process.env.RELOAD);
+};
+// Now this shit is long as fuck
+client.onmessage = function(event) {
+    let data = JSON.parse(event.data);
+    console.log(data);
+    if (data.Identifier != 300) {
+        if (data.Type === 'Generic') {
+            if (['giv', 'kill', 'teleport', 'slay'].some(substring => data.Message.toLowerCase().includes(substring))) {
+                let abuse = new Discord.MessageEmbed()
+                    .setColor(`${process.env.ABUSE_COLOR}`)
+                    .setDescription(data.Message)
+                    .setTimestamp()
+                    .setFooter('ID: Server')
+                c.channels.fetch(process.env.ABUSE_CHAN)
+                    .then(channel => channel.send(abuse));
+            } else if (['mute', 'kick', 'ban'].some(substring => data.Message.toLowerCase().includes(substring))) {
+                let punishment = new Discord.MessageEmbed()
+                    .setColor(`${process.env.PUNISH_COLOR}`)
+                    .setDescription(data.Message)
+                    .setTimestamp()
+                    .setFooter('ID: Server');
+                c.channels.fetch(process.env.PUNISH_CHAN)
+                    .then(channel => channel.send(punishment))
+            } else if (data.Message.startsWith('[Priv')) {
+                let pm = new Discord.MessageEmbed()
+                    .setColor(`${process.env.PRIVATE_COLOR}`)
+                    .setDescription(data.Message)
+                    .setTimestamp()
+                    .setFooter('ID: Private Message does not support ID\'s! :(');
+                c.channels.fetch(process.env.CHAT_CHAN)
+                    .then(channel => channel.send(pm));
+            } else if (data.Message.startsWith('[Better')) {
+                return null;
+            } else {
+                let server = new Discord.MessageEmbed()
+                    .setColor(`${process.env.SERVER_COLOR}`)
+                    .setDescription(data.Message)
+                    .setTimestamp()
+                    .setFooter('ID: Server');
+                c.channels.fetch(process.env.SERVER_CHAN)
+                    .then(channel => channel.send(server));
+            }
+        } else if (data.Type === 'Chat') {
+            let better = JSON.parse(data.Message);
+            if (better.Channel === 0) {
+                let global = new Discord.MessageEmbed()
+                    .setColor(`${process.env.CHAT_COLOR}`)
+                    .setDescription(better.Message)
+                    .setTimestamp()
+                    .setFooter('ID: ' + better.UserId);
+                c.channels.fetch(process.env.CHAT_CHAN)
+                    .then(channel => channel.send(global));
+            } else if (better.Channel === 1) {
+                let team = new Discord.MessageEmbed()
+                    .setColor(`${process.env.TEAM_COLOR}`)
+                    .setDescription(better.Message)
+                    .setTimestamp()
+                    .setFooter('ID: ' + better.UserId);
+                c.channels.fetch(process.env.CHAT_CHAN)
+                    .then(channel => channel.send(team));
+            } else if (better.Channel === 2) {
+                let schat = new Discord.MessageEmbed()
+                    .setColor(`${process.env.SCHAT_COLOR}`)
+                    .setDescription(better.Message)
+                    .setTimestamp()
+                    .setFooter('ID: Server');
+                c.channels.fetch(process.env.CHAT_CHAN)
+                    .then(channel => channel.send(schat));
+            }
+        } else if (data.Type === 'Warning') {
+            let warning = new Discord.MessageEmbed()
+                .setColor(`${process.env.WARNING_COLOR}`)
+                .setDescription(data.Message)
+                .setTimestamp()
+                .setFooter('ID: Server');
+            c.channels.fetch(process.env.WARNING_CHAN)
+                .then(channel => channel.send(warning));
+        } else {
+            return null;
+        }
+    } else {
+        let date = new moment().format('L');
+        let hostinfo = JSON.parse(data.Message);
+        let wipedate = hostinfo.SaveCreatedTime.substring(0, 10);
+        let dateFrom = moment(wipedate).fromNow();
+        let players = hostinfo.Players;
+        let max = hostinfo.MaxPlayers;
+        let queue = hostinfo.Queued;
+
+        function set(ply) {
+            c.user.setActivity(`${ply}`, {
+                type: "STREAMING",
+                url: "https://twitch.tv/a_pink_panther"
+            })
+            c.channels.fetch(process.env.PLAYER_CHAN)
+                // .then(channel => channel.send(`${date} : ${wipedate}`));
+                .then(channel => channel.send(ply));
+        };
+
+        if (process.env.INCLUDE_WIPED_FROM) {
+            if (queue === 0) {
+                let playerCount = `${players} / ${max} wiped ${dateFrom}`;
+                set(playerCount);
+            } else {
+                let playerCount = `${players} / ${max} (${queue}) wiped ${dateFrom}`;
+                set(playerCount);
+            }
+        } else {
+            if (queue === 0) {
+                let playerCount = `${players} / ${max}`;
+                set(playerCount);
+            } else {
+                let playerCount = `${players} / ${max} (${queue})`;
+                set(playerCount);
+            }
+        }
+    }
+}
+
+client.onclose = function(event) {
+    console.log(`Connect to ${url} has been closed., with close event code: ${event.code}!`)
+    c.channels.fetch(process.env.CONNECT_CHAN) // This doesn't work for some reason
+        .then(channel => channel.send(`${process.env.CLOSE_MSG}`));
+};
+client.onerror = function(error) {
+    console.log('WebSocket Error: ' + error);
+};
+
+c.on('ready', () => {
+    console.log(`${c.user.tag} has logged on!`);
+    try {
+        c.user.setActivity(`Server is loading ...`, {
+            type: "STREAMING",
+            url: "https://twitch.tv/apinkpanther"
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+c.login(process.env.TOKEN);
